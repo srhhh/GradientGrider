d0b4fbfd7bc7ce621c5cdaf6e22e2bf69d6927e2
srhhh <rsunttu@gmail.com>
2018-06-07 01:07:49 -1000

    Update f1_functions.f90
    comments until grider

--- f1_functions.f90
+++ f1_functions.f90
@@ -10,14 +10,23 @@ contains
 !This partitions A (rows by cols) and B (rows) by the first column of A
 !Used in quicksort
 subroutine partition(A,B,rows,cols,p,r,m,var)
+!The arguments are (vals,indexer,overcrowdN,Nvar,1,overcrowdN,???,1) from divyup
+
 implicit none
 integer, intent(in) :: p, r, rows, cols, var
 integer, intent(out) :: m
 real, dimension(rows,cols), intent(out) :: A
 integer, dimension(rows,1), intent(out) :: B
+! RS: The name of the variables should be systematic
+! RS: for example, val and var has been used in the code throughoutly for something else
+! RS: use "_" to connect terms to make the name of the variable more intuitive
+! RS: Even thought this is not a priority, please be mindful
 integer :: i, j, i1, i2
+! RS: Did you use i1, i2, and val1 in this subroutine at all?
 real :: val1,val2,val3
 
+! RS: I see you are doing a bubble sort but don't you need to loop through val3 as well?
+! RS: https://www.youtube.com/watch?v=nmhjrI-aW5o
 val3 = A(r,var)
 j = p-1
 do i = p, r-1
@@ -37,6 +46,9 @@ end subroutine partition
 !This swaps two rows (i1 and i2) of a matrix A (rows by cols)
 !Used in partition
 subroutine swapR(A,rows,cols,i1, i2)
+!The arguments are (vals,overcrowdN,Nvar,i,j) 
+!first three args are from from divyup
+!last two args are from partition
 implicit none
 integer, intent(in) :: rows, cols, i1, i2
 real, dimension(rows,cols), intent(out) :: A
@@ -55,8 +67,12 @@ end do
 end subroutine swapR
 
 !This is the same as above but for integers
+! RS: for indexer??
 !Used in partition
 subroutine swapI(A,rows,cols,i1, i2)
+!The arguments are (indexer,overcrowdN,1,i,j) 
+!first three args are from from divyup
+!last two args are from partition
 implicit none
 integer, intent(in) :: rows, cols, i1, i2
 integer, dimension(rows,cols), intent(out) :: A
@@ -79,6 +95,7 @@ end subroutine swapI
 !It's pretty interesting
 !It sorts matrices A and B by the column of A indicated by var
 recursive subroutine qsort(A,B,rows,cols,p,r,var)
+!The arguments are (vals,indexer,overcrowdN,Nvar,1,overcrowdN,1) from divyup
 implicit none
 integer, intent(in) :: rows, cols, p, r, var
 integer :: m
