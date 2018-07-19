d478b4660f4a1456c5acf683ee14b0f0d1fa9347
ka-zuumi <kazuumi@hawaii.edu>
2018-07-18 19:01:11 -1000

    Update 5.2
    did some bug-fixing (I think its solid now)
    also starting on commenting
    
    	modified:   background_job.sh
    	modified:   makeGridwithNewTrajectories.f90

--- playground/background_job.sh
+++ playground/background_job.sh
@@ -5,40 +5,101 @@ echo ""
 echo "Now running the background job"
 echo ""
 
-oldPARAMETERS=PARAMETERS
+###############################################################################################################################################
+###############################################################################################################################################
+
+#Enter the name of the original source code files
+
+#The parameters file
+oldPARAMETERS="PARAMETERS"
+
+#The analysis file
+oldANALYSIS="ANALYSIS"
+
+#The makefile for grid creation
+oldMAKEGRID="make_makeGridwithNewTrajectories2"
+
+#The makefile for grid analysis
+oldMAKEANALYSIS="make_checkNewTrajectorieswithMultipleGrids2"
+
+###############################################################################################################################################
+###############################################################################################################################################
+
+#Enter the name of the source code files to be made in the new directory
+#These must be different from the original source code file names
+
+#The parameters file
 newPARAMETERS=PARAMETERS_new
 
-oldMAKEGRID="make_makeGridwithNewTrajectories2"
-newMAKEGRID=make_makeGridwithNewTrajectories2_new
-
-oldANALYSIS=ANALYSIS
+#The analysis file
 newANALYSIS=ANALYSIS_new
 
-oldMAKEANALYSIS="make_checkNewTrajectorieswithMultipleGrids2"
+#The makefile for grid creation
+newMAKEGRID=make_makeGridwithNewTrajectories2_new
+
+#The makefile for grid analysis
 newMAKEANALYSIS=make_checkNewTrajectorieswithMultipleGrids2_new
 
-##newGRID=0016_00050_0PATH
+###############################################################################################################################################
+###############################################################################################################################################
+
+#Change the below variables for whatever grid you want
+#If you want to change other variables (ex. spacing1) you would
+#have to make more sed statements below to change that
+
+#The ratio of child-level to parent-level cell spacings with respect to var1
 scaling1_0=004
+
+#The ratio of child-level to parent-level cell spacings with respect to var2
 scaling2_0=004
+
+#The number of frames a parent-level cell accepts before it is subdivided
 overcrowd0=00050
+
+#The number of trajectories simulated and added to a new grid
 Ntraj_max=00100
+
+#The number of grids to add to the overall library (folder)
 Ngrid_max=1
 
+###############################################################################################################################################
+###############################################################################################################################################
+
+#Enter what path the original source code is on and what you would like
+#the library housing all the grids (a folder) to be called
+
+#The name of the new library (folder)
 newGRID=testHH2_${scaling1_0}_${scaling2_0}_${overcrowd0}_${Ntraj_max}
+
+#The path that has the original source code
 currentPATH=$(pwd)
+
+#DO NOT TOUCH THESE
 gridPATH=$currentPATH/$newGRID
 newSOURCE=SOURCE
 newPATH=$(pwd)/$newGRID/$newSOURCE
 
+###############################################################################################################################################
+###############################################################################################################################################
+#		GRID CREATION AND ANALYSIS
+###############################################################################################################################################
+###############################################################################################################################################
+
+#Set this true if you want to create a new grid
 if [ "0" -eq "0" ]
 then
 
+#If there is another folder of the same name delete that folder first
 rm -r $currentPATH/$newGRID
+
+#Make the directories, copy all the original source code, etc.
 mkdir $currentPATH/$newGRID
 mkdir $newPATH/
 cp $currentPATH/*.f90 $newPATH/
 cp $currentPATH/make_$(echo "*") $newPATH/
 
+#Make changes to the parameters file as specified in the variables above
+#Unless you want to change MORE variables, don't touch this
 sed "s|Ntraj_max = [0-9]*|Ntraj_max = $Ntraj_max| 
      s|overcrowd0 = [0-9]*|overcrowd0 = $overcrowd0|
      s|scaling1_0 = [0-9]*|scaling1_0 = $scaling1_0|
@@ -52,6 +113,11 @@ sed "s|Ntraj_max = [0-9]*|Ntraj_max = $Ntraj_max|
      s|gridpath0 = .*|gridpath0 = \"$gridPATH/\"|
      s|$oldPARAMETERS\\.f90|$newPARAMETERS.f90|" <$currentPATH/$oldPARAMETERS.f90 >$newPATH/$newPARAMETERS.f90
 
+#Make changes to the analysis file as specified in the variables above
+#This first analysis simulates new trajectories and checks them with the grid
+#All of these variables can be changed;
+#Always remember to replace true with false when changing a logical variable
+#Ntesttraj is how many trajectorie to simulate (important!)
 sed "s/Ngrid_cap = [0-9]*/Ngrid_cap = $Ngrid_max/
      s/heatmap_flag = \\.false\\./heatmap_flag = .true./
      s/trueSA_flag = \\.true\\./trueSA_flag = .false./
@@ -64,18 +130,23 @@ sed "s/Ngrid_cap = [0-9]*/Ngrid_cap = $Ngrid_max/
      s/reject_flag = \\.false\\./reject_flag = .true./
      s/testtrajSA_flag = \\.false\\./testtrajSA_flag = .true./" <$currentPATH/$oldANALYSIS.f90 >$newPATH/$newANALYSIS.f90
 
+#DO NOT TOUCH THIS
 sed "s/$oldPARAMETERS\\.o/$newPARAMETERS.o/
      s/$oldPARAMETERS\\.f90/$newPARAMETERS.f90/
      s|SOURCE = .*|SOURCE = $newPATH/|
      s/$oldANALYSIS\\.o/$newANALYSIS.o/
      s/$oldANALYSIS\\.f90/$newANALYSIS.f90/" <$currentPATH/$oldMAKEGRID >$newPATH/$newMAKEGRID
 
+#DO NOT TOUCH THIS
 sed "s/$oldPARAMETERS\\.o/$newPARAMETERS\\.o/
      s/$oldPARAMETERS\\.f90/$newPARAMETERS\\.f90/
      s|SOURCE = .*|SOURCE = $newPATH/|
      s/$oldANALYSIS\\.o/$newANALYSIS.o/
      s/$oldANALYSIS\\.f90/$newANALYSIS.f90/" <$currentPATH/$oldMAKEANALYSIS >$newPATH/$newMAKEANALYSIS
 
+
+
+#Now, all we need to do is go into the new folder and make the output files
 cd $newGRID/
 
 make -f $newPATH/$newMAKEGRID
@@ -87,6 +158,17 @@ make clean -f $newPATH/$newMAKEANALYSIS
 ./a.out
 
 fi
+
+###############################################################################################################################################
+###############################################################################################################################################
+
+#exit
+
+###############################################################################################################################################
+###############################################################################################################################################
+#		SECOND ANALYSIS
+###############################################################################################################################################
+###############################################################################################################################################
 
 sed "s/Ngrid_cap = [0-9]*/Ngrid_cap = $Ngrid_max/
      s/heatmap_flag = \\.false\\./heatmap_flag = .true./
@@ -110,15 +192,16 @@ make -f $newPATH/$newMAKEANALYSIS
 make clean -f $newPATH/$newMAKEANALYSIS
 ./a.out
 
-
-#################################################################################################################################
-#################################################################################################################################
+###############################################################################################################################################
+###############################################################################################################################################
 
 exit
 
-#################################################################################################################################
-#################################################################################################################################
-
+###############################################################################################################################################
+###############################################################################################################################################
+#		THIRD ANALYSIS
+###############################################################################################################################################
+###############################################################################################################################################
 
 sed "s/Ngrid_cap = [0-9]*/Ngrid_cap = $Ngrid_max/
      s/heatmap_flag = \\.false\\./heatmap_flag = .true./
@@ -142,6 +225,17 @@ make -f $newPATH/$newMAKEANALYSIS
 make clean -f $newPATH/$newMAKEANALYSIS
 ./a.out
 
+###############################################################################################################################################
+###############################################################################################################################################
+
+exit
+
+###############################################################################################################################################
+###############################################################################################################################################
+#		FOURTH ANALYSIS
+###############################################################################################################################################
+###############################################################################################################################################
+
 sed "s/Ngrid_cap = [0-9]*/Ngrid_cap = $Ngrid_max/
      s/heatmap_flag = \\.false\\./heatmap_flag = .true./
      s/trueSA_flag = \\.true\\./trueSA_flag = .false./
@@ -164,6 +258,17 @@ make -f $newPATH/$newMAKEANALYSIS
 make clean -f $newPATH/$newMAKEANALYSIS
 ./a.out
 
+###############################################################################################################################################
+###############################################################################################################################################
+
+exit
+
+###############################################################################################################################################
+###############################################################################################################################################
+#		FIFTH ANALYSIS
+###############################################################################################################################################
+###############################################################################################################################################
+
 sed "s/Ngrid_cap = [0-9]*/Ngrid_cap = $Ngrid_max/
      s/heatmap_flag = \\.false\\./heatmap_flag = .true./
      s/trueSA_flag = \\.true\\./trueSA_flag = .false./
@@ -185,6 +290,17 @@ sed "s/$oldPARAMETERS\\.o/$newPARAMETERS\\.o/
 make -f $newPATH/$newMAKEANALYSIS
 make clean -f $newPATH/$newMAKEANALYSIS
 ./a.out
+
+###############################################################################################################################################
+###############################################################################################################################################
+
+exit
+
+###############################################################################################################################################
+###############################################################################################################################################
+#		SIXTH ANALYSIS
+###############################################################################################################################################
+###############################################################################################################################################
 
 sed "s/Ngrid_cap = [0-9]*/Ngrid_cap = $Ngrid_max/
      s/heatmap_flag = \\.false\\./heatmap_flag = .true./
