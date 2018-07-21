6577c1ce1c78cd327c845f152a7a17c15a783cb6
ka-zuumi <kazuumi@hawaii.edu>
2018-07-20 20:28:52 -1000

    Update 5.6 once more
    yep
    
    	modified:   makeGridwithNewTrajectories.f90

--- playground/makeGridwithNewTrajectories.f90
+++ playground/makeGridwithNewTrajectories.f90
@@ -288,7 +288,7 @@ do Ngrid = 1, Ngrid_max
 			open(gnuplotchannel,file=gridpath1//gnuplotfile)
 			write(gnuplotchannel,*) 'set term jpeg size 1200,1200'
 			write(gnuplotchannel,*) 'set output "'//gridpath1//'checkTrajectory_'//reject_text//Nthreshold_text//&
-			                         checkstateTrajectory//'.jpg"'
+			                         '_'//checkstateTrajectory//'.jpg"'
 			write(gnuplotchannel,*) 'set style line 1 lc rgb "red" pt 5'
 			write(gnuplotchannel,*) 'set style line 2 lc rgb "green" pt 7'
 			write(gnuplotchannel,*) 'set style line 3 lc rgb "blue" pt 13'
